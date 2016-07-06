e38b31a99d1839dc8aabfbc3077ac4143adcbe01
Neosilver-GK <neosilvergk@gmail.com>
2016-07-06 12:28:21 +0200

    Part2 of Tut, add admin-register for Questions

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,33 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        """
+        Funktion to represent the Model
+        """
+        return self.question_text
+
+    def was_published_recently(self):
+        """
+        shows true if pub_date is less then 1 day
+        """
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """
+        Funktion to represent the Model
+        """
+        return self.choice_text
